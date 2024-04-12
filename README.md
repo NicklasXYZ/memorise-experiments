d730a534d4c81bc76e56e4c1f0fe6cd1de37c954
NicklasXYZ <nsa200293@live.dk>
2024-04-12 09:52:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Setup & Run Frontend
+
+Go to frontend directory:
+
+```
+cd frontend
+```
+
+Install javascript/npm libraries:
+
+```
+npm install
+```
+
+Run frontend:
+
+```
+npx vite
+```
+
+# Setup & Run Backend
+
+Go to backend directory:
+
+```
+cd backend
+```
+
+Run locust (`pip install locust`):
+
+```
+locust
+```
+
+
+
+
+
+
+
+
---
+++ frontend/index.js
@@ -0,0 +1,84 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const canvas = document.querySelector('canvas.webgl')
+const scene = new THREE.Scene()
+scene.background =new THREE.Color("rgba(128, 128, 128)");
+
+// Load 3d model here:
+const loader = new GLTFLoader()
+loader.load('assets/base.gltf', function (gltf) {
+    console.log(gltf)
+    const root = gltf.scene;
+    root.scale.set(100, 100, 100)
+    root.position.set(1, -10, 10)
+    scene.add(root)
+}, function (xhr) {
+    console.log((xhr.loaded/xhr.total * 100) + '% loaded')
+}, function (error) {
+    console.log('Error loading')
+})
+
+
+
+
+const light = new THREE.HemisphereLight(0xffffff, "rgba(107, 107, 107)", 1)
+light.position.set(5, 5, 0);
+scene.add(light)
+const lamp = new THREE.AmbientLight(0xffffff, .3)
+lamp.position.set(0, 2, -4);
+scene.add(lamp)
+
+
+const targetObject = new THREE.Object3D();
+scene.add(targetObject);
+
+light.target = targetObject;
+
+
+//Boiler plate code
+const sizes = {
+    width:window.innerWidth,
+    height:window.innerHeight
+}
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
+// camera.position.set(.2, 5, 10)
+camera.position.z = -8;
+camera.position.x = 0;
+camera.position.y = 1;
+camera.rotation.z = .2;
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+ controls.enableZoom = true;
+ controls.enableDamping = false;
+
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true
+})
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap = true
+renderer.gammaOutput = true
+
+window.addEventListener('resize', function(){
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.update()
+})
+
+function animate() {
+    requestAnimationFrame(animate);
+  
+    camera.rotation.z += 0.01;
+  
+  
+    renderer.render(scene, camera);
+  }
+  
+  animate();
+
